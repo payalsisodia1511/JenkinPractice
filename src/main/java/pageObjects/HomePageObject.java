dcd5f963abd4015d64d88e14a8e413c4d72993bb
payalsisodia1511 <payalsisodia1511@gmail.com>
2023-01-27 17:40:23 +0530

    no msg

--- src/main/java/pageObjects/HomePageObject.java
+++ src/main/java/pageObjects/HomePageObject.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+//homepage comment
 public class HomePageObject {
 
 	 WebDriver driver;
